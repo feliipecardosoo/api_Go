60856e153392ccdca477f713f1f4c6e7b39e2e4d
feliipecardosoo <fglibino@gmail.com>
2024-07-05 15:24:00 -0300

    Finalizando Create

--- src/requisicoes/requisicoes.go
+++ src/requisicoes/requisicoes.go
@@ -1,6 +1,7 @@
 package requisicoes
 
 import (
+	"bd/bd"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -22,6 +23,34 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Println(usuario)
+	db, erro := bd.ConexaoBD()
+	if erro != nil {
+		w.Write([]byte("Erro ao insert no banco de dados"))
+		return
+	}
+	defer db.Close()
 
+	// PREPARE STATEMENT
+
+	statement, erro := db.Prepare("insert into usuarios (nome, email) values (?, ?)")
+	if erro != nil {
+		w.Write([]byte("Erro ao criar statement"))
+		return
+	}
+	defer statement.Close()
+
+	insercao, erro := statement.Exec(usuario.Nome, usuario.Email)
+	if erro != nil {
+		w.Write([]byte("Erro ao executar statement"))
+		return
+	}
+
+	idInserido, erro := insercao.LastInsertId()
+	if erro != nil {
+		w.Write([]byte("Erro ao obter id inserido"))
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(fmt.Sprintf("Usuario inserido com sucesso! Id: %d", idInserido)))
 }
