b1ae66e26b053af84786f8a934e55c4a0fa495f7
feliipecardosoo <fglibino@gmail.com>
2024-07-08 22:27:24 -0300

    finalizando comentarios para routes

--- src/server.go
+++ src/server.go
@@ -13,9 +13,15 @@ import (
 func Server() {
 	router := mux.NewRouter()
 
+	// Criacao de Usuario
 	router.HandleFunc("/usuarios", requisicoes.CriarUsuario).Methods(http.MethodPost)
+	// Resgatando Usuarios
 	router.HandleFunc("/usuarios", requisicoes.RetornarAll).Methods(http.MethodGet)
+	// Resgatando Usuario por ID
 	router.HandleFunc("/usuarios/{id}", requisicoes.RetornarUsuario).Methods(http.MethodGet)
+	// Editando Usuario
+
+	// Deletando Usuario
 
 	fmt.Println("Escutando na porta 5k")
 	log.Fatal(http.ListenAndServe(":5000", router))
